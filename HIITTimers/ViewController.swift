bb83fb6437225acf7963feab7413ffee883657b3
Justin Pauga <paugajt@gmail.com>
2019-06-02 15:44:56 -0600

    Initial Commit

---
+++ HIITTimers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HIITTimers
+//
+//  Created by Justin Pauga on 6/2/19.
+//  Copyright © 2019 Justin Pauga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
